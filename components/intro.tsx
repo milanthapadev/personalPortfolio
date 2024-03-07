0da5966afb0e5b7130e3c63b721aaf4dcd166fb4
Milan Thapa <tmilan0604@gmail.com>
2024-03-07 17:12:15 +0545

    update resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Milan_Thapa_CV_2023.pdf"
+          href="/Milan_Thapa_CV_2024.pdf"
           download
         >
           Download CV{" "}
